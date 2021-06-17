3a8daea8651dd422954c262646fa2e359fa8585c
Hien Tran <hien.tran@dkb.de>
2021-06-17 18:06:46 +0200

    Servlet Fehler

--- src/main/java/midterm/BankingApplication.java
+++ src/main/java/midterm/BankingApplication.java
@@ -2,8 +2,11 @@ package midterm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BankingApplication {
 
     public static void main(String[] args) {
